8588d1ae9a8b75643490169df2bb9e3dca46b075
Andrew <heldiggris7@gmail.com>
2017-12-18 11:35:27 -0600

    update What

--- FinalProject/FinalProject/My Project/Resources.Designer.vb
+++ FinalProject/FinalProject/My Project/Resources.Designer.vb
@@ -93,9 +93,49 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property MountainsBackground1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("MountainsBackground1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property num1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("num1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property num2() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("num2", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property treeLevel() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("treeLevel", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property treeLevel1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("treeLevel1", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
@@ -116,6 +156,16 @@ Namespace My.Resources
         Friend ReadOnly Property volcano() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("volcano", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property volcano1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("volcano1", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
